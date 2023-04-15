fd811752c26c1ca90f481e12d61c313ddfc3cf8c
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-04-15 10:57:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Filtering from "./Compoents/Filter2/Filtering";
+import "./styles.css";
+export default function App() {
+  return (
+    <div className="App">
+      <Filtering />
+    </div>
+  );
+}
